4a66f3baa19ef07b809506aa5b6a80a02e7471ac
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2021-09-10 16:03:49 +0530

    made some changes

--- Hello.c
+++ Hello.c
@@ -1,15 +1,30 @@
+// #include <stdio.h>
+// #include <math.h>
+// int main()
+// {
+//     // printf("Hello, World!\n");
+//     // int n = 1234;
+//     // printf("%f", log10(n) + 1);
+//     int n;
+//     scanf("%d", &n);
+//     int arr[n];
+//     for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+//     for (int i = 0; i < n; i++) printf("%d", arr[i]);
+
+//     return 0;
+// }
+
 #include <stdio.h>
-#include <math.h>
+
 int main()
 {
-    // printf("Hello, World!\n");
-    // int n = 1234;
-    // printf("%f", log10(n) + 1);
-    int n;
+    int i = 1, n, fact = 1;
+    printf("HELLO");
     scanf("%d", &n);
-    int arr[n];
-    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
-    for (int i = 0; i < n; i++) printf("%d", arr[i]);
-
-    return 0;
+    while (i <= n)
+    {
+        fact *= i;
+        i++;
+    }
+    printf("%d", fact);
 }
