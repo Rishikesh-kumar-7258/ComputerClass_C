679b9e971e3c27a92c3be5efc338fad9d28d2074
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2022-02-09 16:52:09 +0530

    added insertion sort

---
+++ Sem 4 (DSA)/lab/insertion.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i], j=i-1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j+1] = arr[j];
+            j--;
+        }
+
+        arr[j+1] = key;
+
+    }
+}
+
+int main()
+{
+    int n; scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+
+    insertionSort(arr, n);
+    for (int i = 0; i < n; i++) printf("%d ", arr[i]);
+    printf("\n");
+
+
+    return 0;
+}
