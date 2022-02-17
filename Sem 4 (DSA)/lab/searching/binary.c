ffe151140314a5d4facc928db3ba0860be693719
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2022-02-17 12:17:18 +0530

    Added iterative binary search

--- Sem 4 (DSA)/lab/searching/binary.c
+++ Sem 4 (DSA)/lab/searching/binary.c
@@ -3,7 +3,7 @@
 int binarySearch(int arr[], int l, int h, int target)
 {
     if (l > h) return -1;
-    
+
     int mid = (l + h) / 2;
 
     if (arr[mid] < target) l = mid + 1;
@@ -11,6 +11,33 @@ int binarySearch(int arr[], int l, int h, int target)
     else return mid;
 
     return binarySearch(arr, l, h, target);
+}
+
+int binarSearchIterative(int arr[], int l, int h, int target)
+{
+    int stack[h];
+    int top = -1;
+    stack[++top] = l;
+    stack[++top] = h;
+
+    while (top >= 0)
+    {
+        int high = stack[top--];
+        int low = stack[top--];
+
+        int mid = (low + high) / 2;
+
+        if (arr[mid] > target) high = mid - 1;
+        else if (arr[mid] < target) low = mid + 1;
+        else return mid;
+
+        if (low > high) break;
+
+        stack[++top] = low;
+        stack[++top] = high;
+    }
+
+    return -1;
 }
 
 int main()
@@ -25,7 +52,7 @@ int main()
     printf("Enter the element to find: ");
     int target; scanf("%d", &target);
 
-    int ans = binarySearch(arr, 0, n-1, target);
+    int ans = binarSearchIterative(arr, 0, n-1, target);
 
     if (ans == -1) printf("Not found\n");
     else printf("The element is present at %d\n", ans+1);
