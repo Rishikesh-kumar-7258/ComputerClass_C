436d72ec95b64368bed7d34b4661728546af8498
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2022-02-18 16:21:10 +0530

    Made some changes

--- Sem 4 (DSA)/lab/sorting/merge.c
+++ Sem 4 (DSA)/lab/sorting/merge.c
@@ -83,7 +83,7 @@ int main()
     for (int i = 0; i < n; i++)
         scanf("%d", &arr[i]);
 
-    mergeSortIterative(arr, 0, n - 1);
+    mergeSort(arr, 0, n - 1);
 
     printf("The sorted array is : ");
     for (int i = 0; i < n; i++)
