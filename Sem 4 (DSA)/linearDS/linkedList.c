70b43ea423ebdc235e0390273931bcdbfbb1af6f
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2022-04-15 12:11:27 +0530

    Added linkedList

--- Sem 4 (DSA)/linearDS/linkedList.c
+++ Sem 4 (DSA)/linearDS/linkedList.c
@@ -1,17 +1,83 @@
 #include <stdio.h>
 
-struct Node{
+struct Node
+{
     int val;
-    struct Node* next;
+    struct Node *next;
 };
 
-struct Node* create(int val)
+struct Node *create(int val)
 {
-    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    struct Node *node = (struct Node *)malloc(sizeof(struct Node));
     node->val = val;
     node->next = NULL;
 
     return node;
+}
+
+void insert(struct Node *head, int pos, int val)
+{
+    struct Node *toAdd = create(val);
+
+    if (pos < 1)
+        return;
+
+    if (pos == 1)
+    {
+        toAdd->next = head;
+        head = toAdd;
+
+        return;
+    }
+
+    struct Node *temp = head;
+    int n = 0;
+    while (temp)
+    {
+        n++;
+        temp = temp->next;
+    }
+
+    if (pos > n)
+        return;
+    temp = head;
+    for (int i = 0; i < pos - 1; i++)
+    {
+        temp = temp->next;
+    }
+
+    toAdd->next = temp->next;
+    temp->next = toAdd;
+}
+
+void deleteNode(struct Node *head, int pos)
+{
+    if (pos < 1) return;
+
+    if (pos == 1) 
+    {
+        head = head->next;
+        return;
+    }
+
+    struct Node *temp = head;
+    int n = 0;
+    while (temp)
+    {
+        n++;
+        temp = temp->next;
+    }
+
+    if (pos > n)
+        return;
+
+    for (int i = 0; i < pos - 1; i++)
+    {
+        temp = temp->next;
+    }
+
+    temp->next = temp->next->next;
+
 }
 
 int main()
@@ -21,11 +87,12 @@ int main()
 
 // function to reverse a linked list
 // Iterative method
-struct Node* reverse(struct Node* head)
+struct Node *reverse(struct Node *head)
 {
-    if (!head || !head->next) return head;
+    if (!head || !head->next)
+        return head;
 
-    struct Node* prev, *curr, *next;
+    struct Node *prev, *curr, *next;
     prev = NULL;
     curr = head;
     next = head->next;
@@ -42,11 +109,12 @@ struct Node* reverse(struct Node* head)
 }
 
 // Recursive method
-struct Node* reverse(struct Node* head)
+struct Node *reverse(struct Node *head)
 {
-    if (!head || !head->next) return head;
+    if (!head || !head->next)
+        return head;
 
-    struct Node* ans = reverse(head->next), *temp = ans;
+    struct Node *ans = reverse(head->next), *temp = ans;
 
     while (temp->next)
         temp = temp->next;
